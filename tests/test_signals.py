d7f9763db115af65ffaa0011be82a9e3cab151af
Alex Grönholm <alex.gronholm@nextday.fi>
2018-10-07 19:53:38 +0300

    Improved the reliability of the signals test on Python 3.5

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -3,14 +3,14 @@ import signal
 
 import pytest
 
-from anyio import receive_signals
+from anyio import receive_signals, run_in_thread
 
 
 @pytest.mark.skipif(os.name == 'nt', reason='Signal delivery cannot be tested on Windows')
 @pytest.mark.anyio
 async def test_receive_signals():
     async with receive_signals(signal.SIGUSR1, signal.SIGUSR2) as sigiter:
-        os.kill(os.getpid(), signal.SIGUSR1)
-        os.kill(os.getpid(), signal.SIGUSR2)
+        await run_in_thread(os.kill, os.getpid(), signal.SIGUSR1)
+        await run_in_thread(os.kill, os.getpid(), signal.SIGUSR2)
         assert await sigiter.__anext__() == signal.SIGUSR1
         assert await sigiter.__anext__() == signal.SIGUSR2
