de138a04b2d41ed4390e7d5e15d58e10469132b3
Alex Grönholm <alex.gronholm@nextday.fi>
2019-05-04 13:58:01 -0400

    Fixed mypy error due to undeclared member variable

--- anyio/exceptions.py
+++ anyio/exceptions.py
@@ -1,10 +1,13 @@
 from traceback import format_exception
+from typing import Sequence
 
 
 class ExceptionGroup(BaseException):
     """Raised when multiple exceptions have been raised in a task group."""
 
     SEPARATOR = '----------------------------\n'
+
+    exceptions = ()  # type: Sequence[BaseException]
 
     def __str__(self):
         tracebacks = ['\n'.join(format_exception(type(exc), exc, exc.__traceback__))
