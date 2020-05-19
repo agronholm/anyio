04756ce61f1b4bbc15722771137bc91492316ad7
Matthias Urlichs <matthias@urlichs.de>
2020-05-19 19:06:23 +0200

    Condition needs to accept a Lock

--- anyio/_backends/_trio.py
+++ anyio/_backends/_trio.py
@@ -247,8 +247,8 @@ class Event:
 
 
 class Condition:
-    def __init__(self):
-        self._cond = cond = trio.Condition()
+    def __init__(self, lock: Lock = None):
+        self._cond = cond = trio.Condition(lock=lock)
         self.__aenter__ = cond.__aenter__
         self.__aexit__ = cond.__aexit__
         self.locked = cond.locked
