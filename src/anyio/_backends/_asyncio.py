agronholm/anyio

commit f6987c116a86abbd66451ab44dcb239b683a57c9

Added the acquire() and release() methods to Lock, Condition and Semaphore
Fixes #112.

src/anyio/_backends/_asyncio.py
@@ -596,27 +596,41 @@ async def notify_socket_close(sock: socket.SocketType) -> None:
 # Synchronization
 #
 
-class Lock(asyncio.Lock):
-    async def __aenter__(self):
+class Lock(abc.Lock):
+    def __init__(self):
+        self._lock = asyncio.Lock()
+
+    def locked(self) -> bool:
+        return self._lock.locked()
+
+    async def acquire(self) -> None:
         check_cancelled()
-        await self.acquire()
+        await self._lock.acquire()
 
-    async def __aexit__(self, exc_type: Optional[Type[BaseException]],
-                        exc_val: Optional[BaseException],
-                        exc_tb: Optional[TracebackType]) -> None:
-        self.release()
+    async def release(self) -> None:
+        self._lock.release()
 
 
-class Condition(asyncio.Condition):
-    async def __aenter__(self):
+class Condition(abc.Condition):
+    def __init__(self, lock: Optional[Lock]):
+        asyncio_lock = lock._lock if lock else None
+        self._condition = asyncio.Condition(asyncio_lock)
+
+    async def acquire(self) -> None:
         check_cancelled()
-        return await super().__aenter__()
+        await self._condition.acquire()
+
+    async def release(self) -> None:
+        self._condition.release()
+
+    def locked(self) -> bool:
+        return self._condition.locked()
 
     async def notify(self, n=1):
-        super().notify(n)
+        self._condition.notify(n)
 
     async def notify_all(self):
-        super().notify(len(self._waiters))
+        self._condition.notify_all()
 
     async def wait(self):
         check_cancelled()
@@ -632,14 +646,20 @@ class Event(asyncio.Event):
         return await super().wait()
 
 
-class Semaphore(asyncio.Semaphore):
-    async def __aenter__(self):
+class Semaphore(abc.Semaphore):
+    def __init__(self, value: int):
+        self._semaphore = asyncio.Semaphore(value)
+
+    async def acquire(self) -> None:
         check_cancelled()
-        return await super().__aenter__()
+        await self._semaphore.acquire()
+
+    async def release(self) -> None:
+        self._semaphore.release()
 
     @property
     def value(self):
-        return self._value
+        return self._semaphore._value
 
 
 class Queue(asyncio.Queue):

src/anyio/_backends/_curio.py
@@ -511,16 +511,41 @@ async def notify_socket_close(sock: socket.SocketType):
 # Synchronization
 #
 
-class Lock(curio.Lock):
-    async def __aenter__(self):
+class Lock(abc.Lock):
+    def __init__(self):
+        self._lock = curio.Lock()
+
+    def locked(self) -> bool:
+        return self._lock.locked()
+
+    async def acquire(self) -> None:
         await check_cancelled()
-        return await super().__aenter__()
+        await self._lock.acquire()
+
+    async def release(self) -> None:
+        await self._lock.release()
 
 
-class Condition(curio.Condition):
-    async def __aenter__(self):
+class Condition(abc.Condition):
+    def __init__(self, lock: Optional[Lock]):
+        curio_lock = lock._lock if lock else None
+        self._condition = curio.Condition(curio_lock)
+
+    async def acquire(self) -> None:
         await check_cancelled()
-        return await super().__aenter__()
+        await self._condition.acquire()
+
+    async def release(self) -> None:
+        await self._condition.release()
+
+    def locked(self) -> bool:
+        return self._condition.locked()
+
+    async def notify(self, n=1):
+        await self._condition.notify(n)
+
+    async def notify_all(self):
+        await self._condition.notify_all()
 
     async def wait(self):
         await check_cancelled()
@@ -533,10 +558,20 @@ class Event(curio.Event):
         return await super().wait()
 
 
-class Semaphore(curio.Semaphore):
-    async def __aenter__(self):
+class Semaphore(abc.Semaphore):
+    def __init__(self, value: int):
+        self._semaphore = curio.Semaphore(value)
+
+    async def acquire(self) -> None:
         await check_cancelled()
-        return await super().__aenter__()
+        await self._semaphore.acquire()
+
+    async def release(self) -> None:
+        await self._semaphore.release()
+
+    @property
+    def value(self):
+        return self._semaphore.value
 
 
 class Queue(curio.Queue):

tests/test_synchronization.py
@@ -9,7 +9,7 @@ pytestmark = pytest.mark.anyio
 
 
 class TestLock:
-    async def test_lock(self):
+    async def test_contextmanager(self):
         async def task():
             assert lock.locked()
             async with lock:
@@ -26,7 +26,30 @@ class TestLock:
         assert not lock.locked()
         assert results == ['1', '2']
 
-    async def test_lock_cancel(self):
+    async def test_manual_acquire(self):
+        async def task():
+            assert lock.locked()
+            await lock.acquire()
+            try:
+                results.append('2')
+            finally:
+                await lock.release()
+
+        results = []
+        lock = create_lock()
+        async with create_task_group() as tg:
+            await lock.acquire()
+            try:
+                await tg.spawn(task)
+                await wait_all_tasks_blocked()
+                results.append('1')
+            finally:
+                await lock.release()
+
+        assert not lock.locked()
+        assert results == ['1', '2']
+
+    async def test_cancel(self):
         async def task():
             nonlocal task_started, got_lock
             task_started = True
@@ -77,7 +100,7 @@ class TestEvent:
 
 
 class TestCondition:
-    async def test_condition(self):
+    async def test_contextmanager(self):
         async def notifier():
             async with condition:
                 await condition.notify_all()
@@ -88,6 +111,24 @@ class TestCondition:
                 assert condition.locked()
                 await tg.spawn(notifier)
                 await condition.wait()
+
+    async def test_manual_acquire(self):
+        async def notifier():
+            await condition.acquire()
+            try:
+                await condition.notify_all()
+            finally:
+                await condition.release()
+
+        condition = create_condition()
+        async with create_task_group() as tg:
+            await condition.acquire()
+            try:
+                assert condition.locked()
+                await tg.spawn(notifier)
+                await condition.wait()
+            finally:
+                await condition.release()
 
     async def test_wait_cancel(self):
         async def task():
@@ -114,10 +155,25 @@ class TestCondition:
 
 
 class TestSemaphore:
-    async def test_semaphore(self):
+    async def test_contextmanager(self):
         async def acquire():
             async with semaphore:
                 assert semaphore.value in (0, 1)
+
+        semaphore = create_semaphore(2)
+        async with create_task_group() as tg:
+            await tg.spawn(acquire, name='task 1')
+            await tg.spawn(acquire, name='task 2')
+
+        assert semaphore.value == 2
+
+    async def test_manual_acquire(self):
+        async def acquire():
+            await semaphore.acquire()
+            try:
+                assert semaphore.value in (0, 1)
+            finally:
+                await semaphore.release()
 
         semaphore = create_semaphore(2)
         async with create_task_group() as tg:

src/anyio/_backends/_trio.py
@@ -223,7 +223,19 @@ async def notify_socket_close(sock):
 # Synchronization
 #
 
-Lock = trio.Lock
+class Lock(abc.Lock):
+    def __init__(self):
+        self._lock = trio.Lock()
+
+    def locked(self) -> bool:
+        return self._lock.locked()
+
+    async def acquire(self) -> None:
+        await trio.lowlevel.checkpoint()
+        await self._lock.acquire()
+
+    async def release(self) -> None:
+        self._lock.release()
 
 
 class Event:
@@ -244,15 +256,18 @@ class Event:
         await self._event.wait()
 
 
-class Condition:
+class Condition(abc.Condition):
     def __init__(self, lock: Optional[trio.Lock] = None):
         self._cond = trio.Condition(lock=lock)
 
-    async def __aenter__(self):
-        await self._cond.__aenter__()
+    async def acquire(self) -> None:
+        return await self._cond.acquire()
 
-    async def __aexit__(self, *exc_info):
-        return await self._cond.__aexit__(*exc_info)
+    async def release(self) -> None:
+        self._cond.release()
+
+    def locked(self):
+        return self._cond.locked()
 
     async def notify(self, n: int = 1) -> None:
         self._cond.notify(n)
@@ -260,14 +275,23 @@ class Condition:
     async def notify_all(self) -> None:
         self._cond.notify_all()
 
-    def locked(self):
-        return self._cond.locked()
-
     async def wait(self):
         return await self._cond.wait()
 
 
-Semaphore = trio.Semaphore
+class Semaphore(abc.Semaphore):
+    def __init__(self, value: int):
+        self._semaphore = trio.Semaphore(value)
+
+    async def acquire(self) -> None:
+        await self._semaphore.acquire()
+
+    async def release(self) -> None:
+        self._semaphore.release()
+
+    @property
+    def value(self) -> int:
+        return self._semaphore.value
 
 
 class Queue:

src/anyio/abc.py
@@ -10,15 +10,21 @@ IPAddressType = Union[str, IPv4Address, IPv6Address]
 
 
 class Lock(metaclass=ABCMeta):
-    @abstractmethod
     async def __aenter__(self):
-        pass
+        await self.acquire()
 
-    @abstractmethod
     async def __aexit__(self, exc_type: Optional[Type[BaseException]],
                         exc_val: Optional[BaseException],
-                        exc_tb: Optional[TracebackType]) -> Optional[bool]:
-        pass
+                        exc_tb: Optional[TracebackType]) -> None:
+        await self.release()
+
+    @abstractmethod
+    async def acquire(self) -> None:
+        """Acquire the lock."""
+
+    @abstractmethod
+    async def release(self) -> None:
+        """Release the lock."""
 
     @abstractmethod
     def locked(self) -> bool:
@@ -26,15 +32,21 @@ class Lock(metaclass=ABCMeta):
 
 
 class Condition(metaclass=ABCMeta):
-    @abstractmethod
     async def __aenter__(self):
-        pass
+        await self.acquire()
 
-    @abstractmethod
     async def __aexit__(self, exc_type: Optional[Type[BaseException]],
                         exc_val: Optional[BaseException],
-                        exc_tb: Optional[TracebackType]) -> Optional[bool]:
-        pass
+                        exc_tb: Optional[TracebackType]) -> None:
+        await self.release()
+
+    @abstractmethod
+    async def acquire(self) -> None:
+        """Acquire the underlying lock."""
+
+    @abstractmethod
+    async def release(self) -> None:
+        """Release the underlying lock."""
 
     @abstractmethod
     def locked(self) -> bool:
@@ -76,15 +88,22 @@ class Event(metaclass=ABCMeta):
 
 
 class Semaphore(metaclass=ABCMeta):
-    @abstractmethod
-    async def __aenter__(self):
-        pass
+    async def __aenter__(self) -> 'Semaphore':
+        await self.acquire()
+        return self
 
-    @abstractmethod
     async def __aexit__(self, exc_type: Optional[Type[BaseException]],
                         exc_val: Optional[BaseException],
-                        exc_tb: Optional[TracebackType]) -> Optional[bool]:
-        pass
+                        exc_tb: Optional[TracebackType]) -> None:
+        await self.release()
+
+    @abstractmethod
+    async def acquire(self) -> None:
+        """Decrement the semaphore value, blocking if necessary."""
+
+    @abstractmethod
+    async def release(self) -> None:
+        """Increment the semaphore value."""
 
     @property
     @abstractmethod

commit 77b76e03c45303dc7004b7d34b002e4c596c0f34

Fix Condition.wait on asyncio and curio
Fixes a bug introduced in
https://github.com/agronholm/anyio/commit/f6987c116a86abbd66451ab44dcb239b683a57c9

src/anyio/_backends/_asyncio.py
@@ -640,7 +640,7 @@ class Condition(abc.Condition):
 
     async def wait(self):
         await check_cancelled()
-        return await super().wait()
+        return await self._condition.wait()
 
 
 class Event(abc.Event):

src/anyio/_backends/_curio.py
@@ -554,7 +554,7 @@ class Condition(abc.Condition):
 
     async def wait(self):
         await check_cancelled()
-        return await super().wait()
+        return await self._condition.wait()
 
 
 class Event(abc.Event):

tests/test_synchronization.py
@@ -2,7 +2,7 @@ import pytest
 
 from anyio import (
     create_lock, create_task_group, create_event, create_semaphore, create_condition,
-    open_cancel_scope, wait_all_tasks_blocked, create_capacity_limiter,
+    open_cancel_scope, wait_all_tasks_blocked, create_capacity_limiter, sleep,
     current_default_worker_thread_limiter, CapacityLimiter, create_memory_object_stream)
 from anyio.exceptions import EndOfStream, ClosedResourceError, BrokenResourceError, WouldBlock
 
@@ -136,19 +136,17 @@ class TestCondition:
             task_started = True
             async with condition:
                 await event.set()
-                await event2.wait()
                 await condition.wait()
                 notified = True
 
         task_started = notified = False
         event = create_event()
-        event2 = create_event()
         condition = create_condition()
         async with create_task_group() as tg:
             await tg.spawn(task)
             await event.wait()
+            await sleep(0)
             await tg.cancel_scope.cancel()
-            await event2.set()
 
         assert task_started
         assert not notified