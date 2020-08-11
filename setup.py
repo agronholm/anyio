f7733feaa9c65fdb7d0018454fd0ffe52497f2e1
Alex Grönholm <alex.gronholm@nextday.fi>
2020-08-11 09:58:22 +0300

    Updated packaging configuration

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        'version_scheme': 'post-release',
-        'local_scheme': 'dirty-tag'
-    },
-    setup_requires=[
-        'setuptools_scm >= 1.7.0'
-    ]
-)
+setup()
