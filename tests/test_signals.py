b9812326bed8362b5860bba39c545fd0c0337486
Thomas Grainger <tagrain@gmail.com>
2021-02-22 09:13:31 +0000

    test that process_signal_queue is cancelled

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -6,10 +6,15 @@ import pytest
 
 from anyio import create_task_group, fail_after, open_signal_receiver, run_sync_in_worker_thread
 
-pytestmark = pytest.mark.anyio
+pytestmark = [
+    pytest.mark.anyio,
+    pytest.mark.skipif(
+        sys.platform == 'win32',
+        reason='Signal delivery cannot be tested on Windows',
+    ),
+]
 
 
-@pytest.mark.skipif(sys.platform == 'win32', reason='Signal delivery cannot be tested on Windows')
 async def test_receive_signals():
     async with open_signal_receiver(signal.SIGUSR1, signal.SIGUSR2) as sigiter:
         await run_sync_in_worker_thread(os.kill, os.getpid(), signal.SIGUSR1)
@@ -19,13 +24,26 @@ async def test_receive_signals():
             assert await sigiter.__anext__() == signal.SIGUSR2
 
 
-@pytest.mark.skipif(sys.platform == 'win32', reason='Signal delivery cannot be tested on Windows')
-async def test_task_group_cancellation():
+async def test_task_group_cancellation_open():
     async def signal_handler():
         async with open_signal_receiver(signal.SIGUSR1) as sigiter:
             async for v in sigiter:
-                assert False
+                pytest.fail()
+            pytest.fail()
+        pytest.fail()
 
     async with create_task_group() as tg:
         await tg.spawn(signal_handler)
         await tg.cancel_scope.cancel()
+
+
+async def test_task_group_cancellation_consume():
+    async def consume(sigiter):
+        async for v in sigiter:
+            pytest.fail()
+        pytest.fail()
+
+    async with open_signal_receiver(signal.SIGUSR1) as sigiter:
+        async with create_task_group() as tg:
+            await tg.spawn(consume, sigiter)
+            await tg.cancel_scope.cancel()
