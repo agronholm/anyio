89bab0decc40784b137defd698c66973254d4cda
Thomas Grainger <tagrain@gmail.com>
2021-05-19 10:50:58 +0100

    undo dedent of run_sync_in_worker_thread

--- src/anyio/to_thread.py
+++ src/anyio/to_thread.py
@@ -30,7 +30,7 @@ async def run_sync(
 
 
 async def run_sync_in_worker_thread(
-    func: Callable[..., T_Retval], *args: object, cancellable: bool = False,
+        func: Callable[..., T_Retval], *args: object, cancellable: bool = False,
         limiter: Optional[CapacityLimiter] = None) -> T_Retval:
     warn('run_sync_in_worker_thread() has been deprecated, use anyio.to_thread.run_sync() instead',
          DeprecationWarning)
