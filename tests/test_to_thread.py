7f9edadca9fbeaa7359a553627cb43839457d3d3
Alex Grönholm <alex.gronholm@nextday.fi>
2021-06-15 11:14:48 +0300

    Fixed to_thread.run_sync() hanging on asyncio with run_until_complete() (#307)
    This was due to find_root_task() returning a finished task.
    
    Fixes #304.

--- tests/test_to_thread.py
+++ tests/test_to_thread.py
@@ -183,3 +183,15 @@ def test_asyncio_run_sync_no_asyncio_run(asyncio_event_loop: asyncio.AbstractEve
     asyncio_event_loop.set_exception_handler(exception_handler)
     asyncio_event_loop.run_until_complete(to_thread.run_sync(time.sleep, 0))
     assert not exceptions
+
+
+def test_asyncio_run_sync_multiple(asyncio_event_loop: asyncio.AbstractEventLoop) -> None:
+    """Regression test for #304."""
+    asyncio_event_loop.call_later(0.5, asyncio_event_loop.stop)
+    for _ in range(3):
+        asyncio_event_loop.run_until_complete(to_thread.run_sync(time.sleep, 0))
+
+    for t in threading.enumerate():
+        if t.name == 'AnyIO worker thread':
+            t.join(2)
+            assert not t.is_alive()
