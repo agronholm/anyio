4cd833412a0732cd25fac3fc721d51edbcef0d76
Thomas Grainger <tagrain@gmail.com>
2021-07-27 11:03:54 +0100

    Update src/anyio/from_thread.py

--- src/anyio/from_thread.py
+++ src/anyio/from_thread.py
@@ -35,7 +35,10 @@ def run(func: Callable[..., Coroutine[Any, Any, T_Retval]], *args: object,
         try:
             asynclib = threadlocals.current_async_module
         except AttributeError:
-            raise RuntimeError("This thread wasn't created by an async library, pass kwarg asynclib_token=...")
+            raise RuntimeError(
+                "This thread wasn't created by an async library, "
+                "pass kwarg asynclib_token=..."
+            )
 
         return asynclib.run_async_from_thread(func, *args)
 
