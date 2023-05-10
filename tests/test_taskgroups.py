baaf5bb82f4938112988536d6c48ada7e87c9885
Alex Grönholm <alex.gronholm@nextday.fi>
2023-05-10 23:51:25 +0300

    Added a static type checking test involving TASK_STATUS_IGNORED
    (cherry picked from commit 66dbf1c2a35e2417d535673c779e8fd4f29dc00e)

--- tests/test_taskgroups.py
+++ tests/test_taskgroups.py
@@ -19,6 +19,7 @@ import pytest
 
 import anyio
 from anyio import (
+    TASK_STATUS_IGNORED,
     CancelScope,
     ExceptionGroup,
     create_task_group,
@@ -1185,3 +1186,8 @@ class TestTaskStatusTyping:
         # if these ever stop failing.
         task_status2: TaskStatus[float] = task_status  # type: ignore[assignment]
         task_status2.started(float())
+
+    async def typetest_optional_status(
+        *, task_status: TaskStatus[int] = TASK_STATUS_IGNORED
+    ) -> None:
+        task_status.started(1)
