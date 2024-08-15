9f292692dccd1ecc97c2cb2a5cfad17306ad90f9
Danipulok <danipulok@gmail.com>
2024-08-15 18:51:48 +0300

    Add tests for `MemoryObjectItemReceiver` repr

--- tests/streams/test_memory.py
+++ tests/streams/test_memory.py
@@ -18,7 +18,11 @@ from anyio import (
     wait_all_tasks_blocked,
 )
 from anyio.abc import ObjectReceiveStream, ObjectSendStream, TaskStatus
-from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
+from anyio.streams.memory import (
+    MemoryObjectItemReceiver, 
+    MemoryObjectReceiveStream, 
+    MemoryObjectSendStream,
+)
 
 if sys.version_info < (3, 11):
     from exceptiongroup import ExceptionGroup
@@ -502,3 +506,20 @@ async def test_send_to_natively_cancelled_receiver() -> None:
             await receive_task
 
         assert receive.receive_nowait() == "hello"
+
+
+async def test_memory_object_item_receiver_repr() -> None:
+    """
+    Test the repr of `MemoryObjectItemReceiver`.
+    Since when `item` is not set, the default dataclass repr raises an AttributeError.
+    """
+    receiver = MemoryObjectItemReceiver[str]()
+
+    assert str(receiver) is not None
+    receiver_repr = repr(receiver)
+    assert "item=None" in receiver_repr
+
+    assert str(receiver) is not None
+    receiver.item = "test_item"
+    receiver_repr = repr(receiver)
+    assert "item='test_item'" in receiver_repr
