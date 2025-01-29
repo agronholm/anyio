349478d9862ccd941638979614d980708957d74e
Tobias Alex-Petersen <tobias.petersen@mikrodust.com>
2025-01-29 20:39:42 +0100

    Avoid chaining current exception when exiting task-groups on asyncio (#864)
    Fixes #863.

--- src/anyio/_backends/_asyncio.py
+++ src/anyio/_backends/_asyncio.py
@@ -764,9 +764,13 @@ class TaskGroup(abc.TaskGroup):
 
                 self._active = False
                 if self._exceptions:
+                    # The exception that got us here should already have been
+                    # added to self._exceptions so it's ok to break exception
+                    # chaining and avoid adding a "During handling of above..."
+                    # for each nesting level.
                     raise BaseExceptionGroup(
                         "unhandled errors in a TaskGroup", self._exceptions
-                    )
+                    ) from None
                 elif exc_val:
                     raise exc_val
             except BaseException as exc:
