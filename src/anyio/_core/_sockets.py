de198178f0817aebee5147ecaf2f566dd37545a4
Marcel Telka <marcel@telka.sk>
2025-09-27 09:28:01 +0200

    Update src/anyio/_core/_sockets.py
    Co-authored-by: Alex Grönholm <alex.gronholm@nextday.fi>

--- src/anyio/_core/_sockets.py
+++ src/anyio/_core/_sockets.py
@@ -354,7 +354,7 @@ async def create_tcp_listener(
         local_port or None,
         family=family,
         type=socket.SOCK_STREAM
-        if sys.platform == "win32" or sys.platform == "sunos5"
+        if sys.platform in ("win32", "sunos5")
         else 0,
         flags=socket.AI_PASSIVE | socket.AI_ADDRCONFIG,
     )
