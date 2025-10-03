7c3f196eef39229ac1e0b54c1d81304be4def918
David Brochart <david.brochart@gmail.com>
2025-10-03 11:34:57 +0200

    Test on asyncio and trio

--- tests/test_to_process.py
+++ tests/test_to_process.py
@@ -54,6 +54,7 @@ def process_func(receiver: int, sender: int) -> None:
     sys.platform == "win32",
     reason="The test hangs on Windows",
 )
+@pytest.mark.parametrize("anyio_backend", ["asyncio", "trio"])
 async def test_run_sync_with_kwargs() -> None:
     """
     Test that keyword arguments are passed to the process.
