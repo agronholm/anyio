605f5496d9e7ec5867873e09bb8ab09c89b992fb
Graeme Holliday <graeme.holliday@pm.me>
2026-06-16 12:02:48 -0400

    add race, amap

--- src/anyio/utils.py
+++ src/anyio/utils.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
-__all__ = ("as_completed", "gather")
+__all__ = ("amap", "as_completed", "gather", "race")
 
-from collections.abc import AsyncGenerator, Coroutine
+from collections.abc import AsyncGenerator, Callable, Coroutine, Iterable
 from contextlib import asynccontextmanager
 from typing import Any, TypeVar, overload
 
-from anyio import create_task_group
-from anyio._core._streams import create_memory_object_stream
-from anyio._core._tasks import TaskHandle
+from anyio import (
+    CancelScope,
+    TaskHandle,
+    create_memory_object_stream,
+    create_task_group,
+)
 from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
 
 R = TypeVar("R")
@@ -76,11 +79,14 @@ async def gather(*coros: Coroutine[Any, Any, Any]) -> tuple[Any, ...]:
     """
     Run coroutines concurrently in a task group. The order of result values corresponds
     to the order of coroutines passed.
+
+    :param coros: coroutine objects to run as tasks
+    :return: task results for each argument in the same order as they were passed
+
     """
     handles: list[TaskHandle[Any, Any]] = []
-
     async with create_task_group() as tg:
-        handles.extend([tg.create_task(coro) for coro in coros])
+        handles.extend(tg.create_task(coro) for coro in coros)
 
     return tuple(r.return_value for r in handles)
 
@@ -92,7 +98,13 @@ async def as_completed(
     """
     Run awaitable objects concurrently in a task group, returning an iterator which can
     be used to get result values as they resolve in the order they finish.
+
+    :param coros: coroutine objects to run as tasks
+    :return: MemoryObjectReceiveStream for iterating over results as they resolve.
+
     """
+    if not coros:
+        raise ValueError("as_completed() takes at least one coroutine")
     send, recv = create_memory_object_stream[R]()
 
     async def runner(
@@ -109,3 +121,54 @@ async def as_completed(
             yield recv
         finally:
             tg.cancel_scope.cancel()
+
+
+_sentinel = object()
+
+
+async def race(*coros: Coroutine[Any, Any, T]) -> T:
+    """
+    Run the given coroutines concurrently and return the first one to complete.
+
+    When the first task completes, the remaining tasks are cancelled and their results
+    are discarded. If any task raises an exception, it is propagated to the caller in an
+    exception group.
+
+    :param coros: coroutine objects to run as tasks
+    :return: the return value of the first completed task
+
+    """
+    if not coros:
+        raise ValueError("race() takes at least one coroutine")
+    retval: Any = _sentinel
+
+    async def runner(coro: Coroutine[Any, Any, T], scope: CancelScope) -> None:
+        nonlocal retval
+        local_retval = await coro
+        if retval is _sentinel:
+            retval = local_retval
+            scope.cancel()
+
+    async with create_task_group() as tg:
+        for coro in coros:
+            tg.start_soon(runner, coro, tg.cancel_scope)
+
+    return retval
+
+
+async def amap(
+    func: Callable[[T], Coroutine[Any, Any, R]], args: Iterable[T]
+) -> list[R]:
+    """
+    Run the given coroutine function concurrently for multiple argument values.
+
+    :param func: a coroutine function that takes a single argument
+    :param args: a sequence of argument values to pass to ``func``
+    :return: task results for each argument in the same order as they were passed
+
+    """
+    handles: list[TaskHandle[Any, R]] = []
+    async with create_task_group() as tg:
+        handles.extend(tg.start_soon(func, arg) for arg in args)
+
+    return [h.return_value for h in handles]
