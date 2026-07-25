3dc7d547360869932d865baa3d7504eb90910f7c
Alex Grönholm <alex.gronholm@nextday.fi>
2026-07-25 12:55:40 +0300

    Update tests/test_sockets.py

--- tests/test_sockets.py
+++ tests/test_sockets.py
@@ -409,10 +409,7 @@ class TestTCPStream:
         ),
     )
     async def test_happy_eyeballs_refcycles(
-        self,
-        free_tcp_port: int,
-        anyio_backend_name: str,
-        event_loop_implementation_name: str,
+        self, free_tcp_port: int, anyio_backend_name: str
     ) -> None:
         """
         Test derived from https://github.com/python/cpython/pull/124859
